01275517e52f5efcd3c9f8b57e48fc2d17d36a5b
ArnabBir <arnabbir@gmail.com>
2018-04-15 01:38:40 +0530

    QR simulated

---
+++ test/QR_factorization.cpp
@@ -0,0 +1,36 @@
+#include <stdlib.h>
+#include <time.h>
+#include <limits>
+#include <iostream>
+#include <stdio.h>
+#include <math.h>
+#include <sys/time.h> 
+#include <sys/resource.h>
+#include "../src/matrix/matrix.cpp"
+#include "../src/matmul/matmul.cpp"
+
+using namespace std;
+
+int main(){
+    
+    Matrix  m(256, 256, 0.0), R(256, 256, 0.0), Q(256, 256, 0.0);
+    m.randomize();
+    
+    //cout<<"Matrix 1 : "<<endl;
+    //m.display_matrix();
+
+    struct rusage usage;
+
+    int start_naive_multiplication = clock();
+    m.QR(R, Q);
+    int stop_naive_multiplication = clock();   
+    
+    getrusage(RUSAGE_SELF, &usage);
+    
+    cout<<"resident set size = "<<usage.ru_maxrss<<endl;
+    cout<<"user time = "<<(usage.ru_utime.tv_sec*25600000.0 + usage.ru_utime.tv_usec)/double(CLOCKS_PER_SEC)*25600<<endl;
+    cout<<"sys time = "<<(usage.ru_stime.tv_sec  *25600000.0 + usage.ru_stime.tv_usec)/double(CLOCKS_PER_SEC)*25600<<endl;
+    cout<<"Time = "<<(stop_naive_multiplication - start_naive_multiplication)/double(CLOCKS_PER_SEC)*25600<<endl;    
+    
+    return 0;
+}
